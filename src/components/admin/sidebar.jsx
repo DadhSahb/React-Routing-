c1bd116a5c421ab1b7fe16dcc31fbfe76f3b852c
Hamza Hamid <hamza0995621@gmail.com>
2022-11-10 23:21:11 +0500

    comments

--- src/components/admin/sidebar.jsx
+++ src/components/admin/sidebar.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
+    //adding links
     <ul>
       <li>
         <Link to="/admin/posts">Posts</Link>
